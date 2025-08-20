543fe85a879b0c0623d1a3f58fa1937a6c129892
viniciusclt <viniciusclt@hotmail.com>
2025-08-20 18:22:29 -0300

    ajuste route.ts

--- app/app/api/health/route.ts
+++ app/app/api/health/route.ts
@@ -2,7 +2,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma as db } from '@/lib/db';
 
-
 export async function GET(request: NextRequest) {
   try {
     // Test database connection
